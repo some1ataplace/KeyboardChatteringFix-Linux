6071ce548f7fa32e51cede680df1c3ed86807741
some1ataplace <99353321+some1ataplace@users.noreply.github.com>
2026-05-09 13:55:21 -0400

    Update mouse_chattering.sh

--- mouse_chattering.sh
+++ mouse_chattering.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Change the line below to the absolute path of the folder
 # You can append `--buttons BTN_LEFT,BTN_SIDE` at the very end to ONLY filter those specific buttons.
-
+# (If using modern Python, you may need to run: sudo pip3 install -r requirements.txt --break-system-packages)
 cd </absolute/path/to/folder> && sudo python3 -m src.mouse_main -m <MOUSE id> -t 30
