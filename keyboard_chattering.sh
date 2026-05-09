d413d3f542edd88ccde70930466792093ee480a1
some1ataplace <99353321+some1ataplace@users.noreply.github.com>
2026-05-09 13:53:48 -0400

    Update keyboard_chattering.sh

--- keyboard_chattering.sh
+++ keyboard_chattering.sh
@@ -2,5 +2,4 @@
 # Change the line below to the absolute path of the folder
 # You can append `--keys KEY_A,KEY_SPACE` at the very end to ONLY filter those specific keys.
 # (If using modern Python, you may need to run: sudo pip3 install -r requirements.txt --break-system-packages)
-
-cd <absolute/path/to/folder> && sudo python3 -m src -k <KEYBOARD id> -t <OPTIONAL, 30 is the default threshold>
+cd </absolute/path/to/folder> && sudo python3 -m src.keyboard_main -k <KEYBOARD id> -t 30
